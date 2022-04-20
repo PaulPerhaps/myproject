0ce79d31513d645d18d4f5cdf35c6b05f560a93b
paulperhaps <pavelukkh@gmail.com>
2022-04-20 15:51:05 -0400

    сÐфÐadded url for login page, added function directing to login.html, added loging.html file, added css file with images

--- conf/urls.py
+++ conf/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.login, name='login'),
 ]
 
 if settings.DEBUG:
