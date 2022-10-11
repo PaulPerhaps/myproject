3b00cef9eedf3a8205d899bd969cb34f25e2202c
paulperhaps <pavelukkh@gmail.com>
2022-10-11 11:43:58 -0400

    api allows admin to view/edit users

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.models import Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from apps.users.serializers import UserSerializer, GroupSerializer
 
 
 # Create your views here.
@@ -45,3 +49,36 @@ def zoom(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def profile_page_m(request):
+    return render(request, "profile_page_m.html")
+
+
+def forgot_p(request):
+    return render(request, "forgot_p.html")
+
+
+def create_a(request):
+    return render(request, "create_a.html")
+
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- conf/settings.py
+++ conf/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.users',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
